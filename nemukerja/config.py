e6b869efa5c6fcb34b2a4dd5b720dff7d0c704ab
Ayamet <ayamet2029@gmail.com>
2025-10-08 19:11:22 +0300

    tmrw will be back

--- nemukerja/config.py
+++ nemukerja/config.py
@@ -1,8 +1,7 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
